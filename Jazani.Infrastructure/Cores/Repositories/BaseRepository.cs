6b67198c4b0c96792e68b37c84d1afbad4c3d61d
david3vc <dveracabanillas@hotmail.com>
2023-10-07 10:51:33 -0500

    Start IBaseRepository

---
+++ Jazani.Infrastructure/Cores/Repositories/BaseRepository.cs
@@ -0,0 +1,68 @@
+﻿using Jazani.Domain.Cores.Models;
+using Jazani.Domain.Cores.Repositories;
+using Jazani.Infrastructure.Cores.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jazani.Infrastructure.Cores.Repositories
+{
+    public abstract class BaseRepository<T, ID> : IBaseRepository<T, ID> where T : CoreModel<ID>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private DbSet<T> _dbSet;
+        protected BaseRepository(ApplicationDbContext dbContext)
+        {
+            _dbSet = dbContext.Set<T>();
+            _dbContext = dbContext;
+        }
+
+        public virtual async Task<T> DisableAsync(ID id)
+        {
+            T? entity = await _dbSet.FindAsync(id);
+
+            if (entity == null) throw new Exception("No se encontro informacion para el id: " + id);
+
+            entity.State = false;
+            _dbSet.Update(entity);
+
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<IReadOnlyList<T>> FindAllAsync()
+        {
+            return await _dbSet
+                .Where(x => x.State == true)
+                .ToListAsync();
+        }
+
+        public Task<T?> FindByIdAsync(ID id)
+        {
+            //return await _dbSet.FindAsync(id);
+
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SaveAsync(T entity)
+        {
+            //EntityState state = _dbContext.Entry(entity).State;
+
+            //_ = state switch
+            //{
+            //    EntityState.Detached => _dbSet.Add(entity),
+            //    EntityState.Modified => _dbSet.Update(entity)
+            //};
+
+
+            //await _dbContext.SaveChangesAsync();
+
+            //return entity;
+            throw new NotImplementedException();
+        }
+    }
+}
